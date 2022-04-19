33484090cc5e2a88d0021bd84e70f36959f79a03
Shreedhar <meshree4@gmail.com>
2022-04-19 21:46:53 +0530

    Can book a slot

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-
-from argparse import ArgumentError
-from pydoc import render_doc
-from tkinter import E
-from wsgiref.util import request_uri
-
-# from firebase_admin import credentials
-# from firebase_admin import auth
+import datetime
 from flask import Flask, render_template, redirect, request, request_tearing_down, session, url_for
 import re
 from idna import valid_label_length
 import pyrebase
 from requests import Session
-
+import re
+import time
 
 
 try:
@@ -20,15 +14,22 @@ try:
 
     firebaseConfig = {
    
-    }
+  }
+
 
     firebase = pyrebase.initialize_app(firebaseConfig)
     auth = firebase.auth()
-
+    db = firebase.database()    
+    status = ""
     @app.route("/")
 
     def index():
-        return render_template('index.html')
+        global status   
+        status = db.child("sensor_value").get().val()
+        status = "green" if status == "0" else "red"
+        print(status)
+        
+        return render_template('index.html', status = status)
 
     @app.route("/register", methods = ["GET", "POST"])
     def register():
@@ -46,7 +47,7 @@ try:
                 elif cpassword != password:
                     return render_template('register.html', msg = "Confirm password doesn't match with password!!")
 
-                elif not session['uid']:    
+                else:    
                     auth.create_user_with_email_and_password(email = email, password= password)
 
                     return render_template('login.html', msg = 'Account created successfully!!!')
@@ -70,6 +71,7 @@ try:
     def login():
 
         try:
+            global status
             msg = ''
             
             if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
@@ -80,14 +82,14 @@ try:
                 
                 
                 if user:
-                    session['uid'] = user
-
-                    return render_template('index.html', msg = 'Logged in successfully')
+                    session['uid'] = user['email'][:-10]
+                   
+                    return render_template('index.html', msg = 'Logged in successfully', status = status)
                 
                 else:
                     return render_template('login.html', msg = 'Invalid email or password')
             
-            if not session['uid'] and request.method == "GET" :
+            if  request.method == "GET" :
                 print("inside normal request")
                 
                 return render_template('login.html')
@@ -112,7 +114,7 @@ try:
         
         except Exception as e:
             print(e)
-            return render_template('index.html', msg = e)
+            return render_template('index.html', msg = e, status = status)
 
     @app.route('/logout')
     def logout():
@@ -120,10 +122,57 @@ try:
         session.pop('uid', None)
         return render_template('login.html', msg = 'Logged out successfully!!')
 
+    @app.route('/booking', methods = ["GET", "POST"])
+    def booking():
+        try:
+            if session['uid'] and request.method == "POST" and 'vno' in request.form and 'time' in request.form: 
+                dt = str(datetime.datetime.now())[:11]
+                vehicle_no  = request.form['vno']
+                duration = request.form['time']
+                
+                
+                bookings = db.child("bookings").child(session['uid']).get()    
+                data = { ("date") : dt, ("vehicle_no" ) : vehicle_no, ("duration") : duration}
+                db.child("bookings").child(session['uid']).push(data)
+                bookings = db.child("bookings").child(session['uid']).get()    
+                global status
+                status = "red"
+                return render_template("my_bookings.html", 
+                                        msg = "Booked a slot!!",
+                                        )
+            
+           
+        except Exception as e:
+            print(e)
+            return render_template("book_slot.html", msg = e)  
+
+    @app.route('/my_bookings.html')
+    def my_bookings():
+            
+        try:
+            if session['uid']:
+
+                bookings = db.child("bookings").child(session['uid']).get()    
+                
+                my_bookings = dict()
+                for booking in bookings.each():
+                    my_bookings[booking.key()] = {'vehicle_no': booking.val()['vehicle_no'], 
+                                                'duration': booking.val()['duration'], 
+                                                'date': booking.val()['date']
+                                                }
+                return render_template("my_bookings.html", my_bookings = my_bookings)
+
+            else:
+                return render_template("login.html", msg = "Please login first!!!")
+            
+        except Exception as e:
+            print(e)
+            return render_template("my_bookings.html", msg = e)
+
 except Exception as e:
     print(e)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port = 5000, debug=True)
     
